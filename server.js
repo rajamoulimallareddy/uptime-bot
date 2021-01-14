ba6979d29e3b89c29122fb5a41a9eeb26be7d6c0
Navaneeth K M <34748927+navaneethkm004@users.noreply.github.com>
2021-01-14 13:16:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,99 +1,112 @@
 require("express")().listen(1343);
 
-const db = require("quick.db"); 
+const db = require("quick.db");
 const discord = require("discord.js");
 const client = new discord.Client({ disableEveryone: true });
-client.login("TOKEN HERE!!");
+client.login("TOKEN");
 const fetch = require("node-fetch");
-const fs = require('fs')
+const fs = require("fs");
 
 setInterval(() => {
   var links = db.get("linkler");
-  if(!links) return 
-  var linkA = links.map(c => c.url)
+  if (!links) return;
+  var linkA = links.map(c => c.url);
   linkA.forEach(link => {
     try {
-      fetch(link)
-    } catch(e) { console.log("" + e) };
-  })
-  console.log("Pinged Successfully.")
-}, 60000)
+      fetch(link);
+    } catch (e) {
+      console.log("" + e);
+    }
+  });
+  console.log("Pinged Successfully.");
+}, 60000);
 
 client.on("ready", () => {
-if(!Array.isArray(db.get("linkler"))) {
-db.set("linkler", [])
-}
-})
-
-client.on("ready", () => {
-  client.user.setActivity(`up-help | Created By Navaneeth K M </>#7174`)
-  console.log(`Logined`)
-})
-
-
-client.on("message", message => {
-  if(message.author.bot) return;
-  var spl = message.content.split(" ");
-  if(spl[0] == "up-uptime") {
-  var link = spl[1]
-  fetch(link).then(() => {
-    if(db.get("linkler").map(z => z.url).includes(link)) return message.channel.send("**⛔ This project is already uptimed!!**")
-    
-    let NewHelp = new Discord.RichEmbed()
-        .setAuthor(client.user.username, client.user.avatarURL)
-        .setColor(0x6A3DB8)
-        .setDescription("**✅ Successfully added to the pinging list !!!**")
-        .setFooter(`© ${client.user.username}`, client.user.avatarURL)
-        .setTimestamp()
-     message.channel.send(NewHelp).then(msg => msg.delete(60000)); //Clears the response after 60000/60 seconds
-    db.push("linkler", { url: link, owner: message.author.id})
-  }).catch(e => {
-    let NewHelp = new Discord.RichEmbed()
-        .setAuthor(client.user.username, client.user.avatarURL)
-        .setColor(0x6A3DB8)
-        .setDescription("⛔ **Error!! Only Absolute URLs are accepted.**")
-        .setFooter(`© ${client.user.username}`, client.user.avatarURL)
-        .setTimestamp()
-   return message.channel.send(NewHelp).then(msg => msg.delete(60000)); //Clears the response after 60000/60 seconds
-  })
+  if (!Array.isArray(db.get("linkler"))) {
+    db.set("linkler", []);
   }
-})
+});
 
+client.on("ready", () => {
+  client.user.setActivity(`up-help | Created By Navaneeth K M </>#7174`);
+  console.log(`Logined`);
+});
 
 client.on("message", message => {
-  if(message.author.bot) return;
+  if (message.author.bot) return;
   var spl = message.content.split(" ");
-  if(spl[0] == "up-total") {
-  var link = spl[1]
- message.channel.send(`**${db.get("linkler").length} / 1000**`)
-}})
+  if (spl[0] == "up-uptime") {
+    var link = spl[1];
+    fetch(link)
+      .then(() => {
+        if (
+          db
+            .get("linkler")
+            .map(z => z.url)
+            .includes(link)
+        )
+          return message.channel.send(
+            "**⛔ This project is already uptimed!!**"
+          );
 
-
-
-const Discord = require('discord.js');
+        let yardım = new Discord.RichEmbed()
+          .setAuthor(client.user.username, client.user.avatarURL)
+          .setColor(0x6a3db8)
+          .setDescription("**✅ Successfully added to the pinging list !!!**")
+          .setFooter(`© ${client.user.username}`, client.user.avatarURL)
+          .setTimestamp();
+        message.channel.send(yardım).then(msg => msg.delete(60000)); //Clears the response after 60000/60 seconds
+        db.push("linkler", { url: link, owner: message.author.id });
+      })
+      .catch(e => {
+        let yardım = new Discord.RichEmbed()
+          .setAuthor(client.user.username, client.user.avatarURL)
+          .setColor(0x6a3db8)
+          .setDescription("⛔ **Error!! Only Absolute URLs are accepted.**")
+          .setFooter(`© ${client.user.username}`, client.user.avatarURL)
+          .setTimestamp();
+        return message.channel.send(yardım).then(msg => msg.delete(60000)); //Clears the response after 60000/60 seconds
+      });
+  }
+});
 
 client.on("message", message => {
-  if(message.author.bot) return;
-    var spl = message.content.split(" ");
-  if(spl[0] == "up-help") {
-let embed = new Discord.RichEmbed()
-.setColor('#070706')
-.setDescription(`**Commands**
+  if (message.author.bot) return;
+  var spl = message.content.split(" ");
+  if (spl[0] == "up-total") {
+    var link = spl[1];
+    message.channel.send(`**${db.get("linkler").length} / 1000**`);
+  }
+});
+
+const Discord = require("discord.js");
+
+client.on("message", message => {
+  if (message.author.bot) return;
+  var spl = message.content.split(" ");
+  if (spl[0] == "up-help") {
+    let embed = new Discord.RichEmbed()
+      .setColor("#070706")
+      .setDescription(
+        `**Commands**
 
  🪐 **up-help**  | Shows all commands (This page) 🪐
-
- 💎 **up-uptime**  | Uptimes your project/bot 💎
-
- 🧿 **up-total** | Shows the total projects/bots uptimed 🧿
-
-`)
-.setAuthor(`Navaneeth K M`, client.user.avatarURL)
-.setFooter(`Navaneeth's Uptime Bot | Created By Navaneeth K M </>#7174`, client.user.avatarURL)
-return message.channel.send(embed);
-    }
  
-})
+ 💎 **up-uptime**  | Uptimes your project/bot 💎
+ 
+ 🧿 **up-total** | Shows the total projects/bots uptimed 🧿
+ 
+`
+      )
+      .setAuthor(`Uptime`, client.user.avatarURL)
+      .setFooter(
+        `Navaneeth's Uptime Bot | Created By Navaneeth K M </>#7174`,
+        client.user.avatarURL
+      );
+    return message.channel.send(embed);
+  }
+});
 
-  const log = message => {
+const log = message => {
   console.log(`${message}`);
-}
+};
